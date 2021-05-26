ee96ec444172883652d3489c1e643d31b6aae5cd
MahmudJion <mahmudh.jion02@gmail.com>
2021-05-26 18:01:00 +0600

    route update with api folder

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,7 +30,9 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->resource('employee');
+$routes->resource('employee', [
+    'websafe' => 1,
+]);
 
 /**
  * --------------------------------------------------------------------
