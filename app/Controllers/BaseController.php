e71bf0c278d43989b714e8189faf2bb54dabae4c
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2021-08-22 15:38:16 +0600

    update BaseController.php

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -1,5 +1,14 @@
 <?php
+
 namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use \Firebase\JWT\JWT;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use App\Libraries\Uuid;
 
 /**
  * Class BaseController
@@ -10,15 +19,10 @@ namespace App\Controllers;
  *     class Home extends BaseController
  *
  * For security be sure to declare any new methods as protected or private.
- *
- * @package CodeIgniter
  */
 
-use CodeIgniter\Controller;
-
-class BaseController extends Controller
+class BaseController extends ResourceController
 {
-
 	/**
 	 * An array of helpers to be loaded automatically upon
 	 * class instantiation. These helpers will be available
@@ -30,8 +34,12 @@ class BaseController extends Controller
 
 	/**
 	 * Constructor.
+	 *
+	 * @param RequestInterface  $request
+	 * @param ResponseInterface $response
+	 * @param LoggerInterface   $logger
 	 */
-	public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
+	public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
 	{
 		// Do Not Edit This Line
 		parent::initController($request, $response, $logger);
@@ -39,8 +47,69 @@ class BaseController extends Controller
 		//--------------------------------------------------------------------
 		// Preload any models, libraries, etc, here.
 		//--------------------------------------------------------------------
-		// E.g.:
-		// $this->session = \Config\Services::session();
+		// E.g.: $this->session = \Config\Services::session();
 	}
 
+    protected static function buildResponse($type = 'success', $data = null, $message = null, $limit = null, $offset = null, $total = null)
+    {
+        $res = [
+            'type' => $type
+        ];
+
+        if ($data != null) {
+            $res['data'] = $data;
+        }
+
+        if ($message != null) {
+            $res['message'] = $message;
+        }
+
+        if ($limit != null) {
+            $res['limit'] = $limit;
+        }
+
+        if ($offset != null) {
+            $res['offset'] = $offset;
+        }
+
+        if ($total != null) {
+            $res['total'] = $total;
+        }
+
+        return $res;
+    }
+
+    public function getUniqueID($prefix = false)
+    {
+        $this->uuid = new Uuid();
+        $prefixName = !empty($prefix) ? $prefix . '-' : '';
+        return $prefixName . $this->uuid->v4();
+    }
+
+    protected function verifyToken($token = '')
+    {
+        if ($token) {
+            $token = $token;
+        } else {
+            $token = $this->request->getHeaderLine('Token');
+        }
+        if ($token) {
+            $key = $_ENV['JWT_SECRET'];
+            try {
+                $decoded = JWT::decode($token, $key, array('HS256'));
+                return $decoded;
+            } catch (\Exception $e) {
+                // log the error echo $e->getmessage();
+                $res = new \stdClass();
+                $res->error = true;
+                $res->message = $e->getmessage();
+                return $res;
+            }
+        } else {
+            $res = new \stdClass();
+            $res->error = true;
+            $res->message = 'Token not found';
+            return $res;
+        }
+    }
 }
