cd3484a5446dc119f1e5c3f16a853777f5fd35fb
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2025-04-07 13:23:26 +0100

    refactor EmployeeModel: enhance type hinting and improve documentation for model properties

--- app/Models/EmployeeModel.php
+++ app/Models/EmployeeModel.php
@@ -4,7 +4,29 @@ use CodeIgniter\Model;
 
 class EmployeeModel extends Model
 {
-    protected $table = 'users';
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['name', 'email'];
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected string $table = 'users';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected string $primaryKey = 'id';
+
+    /**
+     * The fields that are allowed to be inserted or updated.
+     *
+     * @var array
+     */
+    protected array $allowedFields = [
+        'name',
+        'email',
+        'created_at',
+        'updated_at',
+    ];
 }
