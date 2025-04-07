5f15912078822b4e369f9b89bcbf055b17892253
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2025-04-07 13:23:22 +0100

    refactor AuthModel: enhance code structure with improved type hinting and detailed documentation

--- app/Models/AuthModel.php
+++ app/Models/AuthModel.php
@@ -6,18 +6,78 @@ use CodeIgniter\Model;
 
 class AuthModel extends Model
 {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected string $table = 'customer';
 
-    protected $table = 'customer';
-    protected $primaryKey = 'id';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected string $primaryKey = 'id';
 
-    protected $allowedFields = [
-        'id', 'name', 'email','password','status', 'created_at', 'modified_at', 'first_name', 'last_name', 'profile_picture', 'gender', 'age', 'height', 'weight', 'diet_plan_list', 'dietary_goals', 'ethical_rating', 'diet_restriction'
+    /**
+     * The fields that are allowed to be inserted or updated.
+     *
+     * @var array
+     */
+    protected array $allowedFields = [
+        'id',
+        'name',
+        'email',
+        'password',
+        'status',
+        'created_at',
+        'modified_at',
+        'first_name',
+        'last_name',
+        'profile_picture',
+        'gender',
+        'age',
+        'height',
+        'weight',
+        'diet_plan_list',
+        'dietary_goals',
+        'ethical_rating',
+        'diet_restriction',
     ];
-    protected $createdField = 'created_at';
-    protected $updatedField = 'modified_at';
 
-    protected $validationRules = [];
-    protected $validationMessages = [];
-    protected $skipValidation = false;
+    /**
+     * The name of the created timestamp field.
+     *
+     * @var string
+     */
+    protected string $createdField = 'created_at';
 
+    /**
+     * The name of the updated timestamp field.
+     *
+     * @var string
+     */
+    protected string $updatedField = 'modified_at';
+
+    /**
+     * Validation rules for the model.
+     *
+     * @var array
+     */
+    protected array $validationRules = [];
+
+    /**
+     * Custom validation messages.
+     *
+     * @var array
+     */
+    protected array $validationMessages = [];
+
+    /**
+     * Whether to skip validation.
+     *
+     * @var bool
+     */
+    protected bool $skipValidation = false;
 }
